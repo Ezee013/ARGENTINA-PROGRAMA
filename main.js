d41d70077db9e43c6a8fa335aa02012a9d501334
Ezequiel <ezequiel2014.edlv@gmail.com>
2022-12-21 14:12:05 -0300

    proyecto terminado, html css js

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
-function boton {
-    alert("Hello wrld");
-}
+ScrollReveal().reveal('.portada-dw');
+ScrollReveal().reveal('.secciones', { delay: 350 });
+ScrollReveal().reveal('.portada-dm', { delay: 350 });
+ScrollReveal().reveal('.portada-ia', { delay: 350 });
+ScrollReveal().reveal('.seccion-ia', { delay: 350 });
